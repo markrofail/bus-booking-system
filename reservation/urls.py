8ff5a9bf48067fbc94b219f58c81733e1d71df2d
Mark Rofail <markm.rofail@gmail.com>
2020-05-01 17:51:08 +0200

    [feat] add /stations endpoint

--- reservation/urls.py
+++ reservation/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'v1'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('stations', views.stations_list, name='stations-list'),
 ]
--- reservation/views.py
+++ reservation/views.py
@@ -1,5 +1,18 @@
-from django.http import JsonResponse
+from rest_framework import permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+from .models import BusStation
+from .serializers import BusStationSerializer
 
 
-def index(request):
-    return JsonResponse({'healthCheck': 'ok'})
+@api_view(['GET'])
+@permission_classes((permissions.AllowAny,))
+def stations_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        stations = BusStation.objects.all()
+        serializer = BusStationSerializer(stations, many=True)
+        return Response(serializer.data)
