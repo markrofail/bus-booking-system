f1da53bec66232812544942cddf1288b0a049c2f
Mark Rofail <markm.rofail@gmail.com>
2020-05-01 11:27:09 +0200

    [fix] rename project to api to avoid name conflict with apps

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for busreservation project.
+WSGI config for api project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'busreservation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = get_wsgi_application()
