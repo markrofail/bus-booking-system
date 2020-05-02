7c630b86770f41787fd653c022f0cfa70457581f
Mark Rofail <markm.rofail@gmail.com>
2020-05-02 12:02:33 +0200

    [feat] add reservation app Admin page

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bus, BusStation
+from .models import Trip, TripRoute, TripStop
+from .models import Reservation
+
+admin.site.register(Bus)
+admin.site.register(BusStation)
+admin.site.register(Trip)
+admin.site.register(Reservation)
+
+
+class TripStopInline(admin.TabularInline):
+    model = TripStop
+    extra = 1
+
+
+class TripRouteAdmin(admin.ModelAdmin):
+    inlines = [TripStopInline]
+
+
+admin.site.register(TripRoute, TripRouteAdmin)
