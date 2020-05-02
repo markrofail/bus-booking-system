775b5459bdc8c827f60bb3052c9f76f472c8d298
Mark Rofail <markm.rofail@gmail.com>
2020-05-02 15:22:35 +0200

    [test] add GET `/stations` endpoint

--- reservation/tests.py
+++ reservation/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.test import Client, TestCase
+
+from .models import BusStation
+
+
+class StationsEndPointTests(TestCase):
+    fixtures = ['busstations']
+
+    def testAllStations(self):
+        station_count = BusStation.objects.all().count()
+
+        response = self.client.get('/api/v1/stations')
+        self.assertEqual(response.status_code, 200)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), station_count)
+
 
 # Create your tests here.
