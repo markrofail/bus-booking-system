24f1bd8163c4a1a4f15729c6266c65ca1ecc87e0
Mark Rofail <markm.rofail@gmail.com>
2020-05-02 15:22:51 +0200

    [test] add GET `/trips` endpoint test

--- reservation/tests.py
+++ reservation/tests.py
@@ -1,4 +1,5 @@
-from django.test import TestCase
+from datetime import datetime
+
 from django.test import Client, TestCase
 
 from .models import BusStation
@@ -16,5 +17,22 @@ class StationsEndPointTests(TestCase):
         response_json = response.json()
         self.assertEqual(len(response_json), station_count)
 
+class TripsEndPointTests(TestCase):
+    fixtures = ['busstations', 'buses', 'triproutes', 'tripstops', 'trips']
 
-# Create your tests here.
+    def testQueryTrips(self):
+        query_params = dict()
+        query_params['date_from'] = datetime(2020, 1, 17, 0, 0, 0, 0)
+        query_params['date_to'] = datetime(2020, 1, 19, 0, 0, 0, 0)
+
+        query_params['start_station'] = BusStation.objects.get(name="Asyut")
+        query_params['end_station'] = BusStation.objects.get(name="Banha").id
+
+        response = self.client.get('/api/v1/trips', query_params)
+        print(response.json())
+        self.assertEqual(response.status_code, 200)
+
+        response_json = response.json()
+        self.assertEqual(len(response_json), 1)
+        trip_name = response_json[0]['name']
+        self.assertEqual(trip_name, "Trip Zentry")
