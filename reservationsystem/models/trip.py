17e9190d27daa084c7f63c3e55d1b5a1a734976f
Mark Rofail <markm.rofail@gmail.com>
2020-05-03 06:51:12 +0200

    [fix] remove `last_reserved_seat` field from Trip model

--- reservationsystem/models/trip.py
+++ reservationsystem/models/trip.py
@@ -7,7 +7,5 @@ class Trip(models.Model):
     trip_route = models.ForeignKey('reservationsystem.triproute', on_delete=models.PROTECT)
     departure_time = models.DateTimeField('departure time')
 
-    last_reserved_seat = models.IntegerField(default=0, editable=False)
-
     def __str__(self):
         return self.name
